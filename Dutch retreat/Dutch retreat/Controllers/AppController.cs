37167b39853923046f99de16330fdb98eab08d49
Suleman Sani <sulemansani26@gmail.com>
2021-04-06 17:58:14 +0100

    new view added to the project

---
+++ Dutch retreat/Dutch retreat/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dutch_retreat.Controllers
+{
+    public class AppController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Dutch retreat/Dutch retreat/wwwroot/js/index.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+
+    console.log("Hi suleman");
+
+    var theform = jQuery("#theform");
+    theform.hide();
+
+    var button = $("#buybutton");
+    button.on("click", function () {
+        console.log("buying item");
+    });
+
+    var productinfo = $(".product-props li");
+    productinfo.on("click", function () {
+        console.log("You cliked on " + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $popupForm.on("click", function () {
+        console.log("this one worked pop")
+    });
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+
+
+});
